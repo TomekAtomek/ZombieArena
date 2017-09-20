8511070778dbc2fa979ce6e4eb71195cc871f440
TomekAtomek <woznica.tomek@gmail.com>
2017-09-20 15:09:44 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
